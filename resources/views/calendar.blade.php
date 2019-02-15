3016b454b2dc3888b82a5d1b26302f5ab9362d19
EC2 Default User <ec2-user@ip-172-31-8-123.ec2.internal>
2019-02-15 23:50:00 +0000

    update calendar

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -23,6 +23,9 @@
             // 方法１：mktimeを使う mktime(hour,minute,second,month,day,year)
             $prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
             $next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
+            $y = substr($ym, 0, 4);//date('Y');
+            $m = substr($ym, 5, 2);//date('m');
+            //echo $prev."<br>".$ym."<br>".$next."<br>";
         ?>
         <div class="schedule_money">
             <!--<p><i class="far fa-calendar schedule_select selected"></i></p>-->
@@ -37,16 +40,19 @@
             <tr class='calendar'>
                 @foreach ($expenses as $expense)
                     <?php
-                        $y = substr($ym, 0, 4);//date('Y');
-                        $m = substr($ym, 5, 2);//date('m');
                         $d = 1;
                         $today = date('Y-m-j');
-                     
                         // 1日の曜日の前に空白を表示
+                        //$wd1には今月の１日の曜日が入る
                         $wd1 = date("w", mktime(0, 0, 0, $m, 1, $y));
+                        $last_day = date('t', mktime(0, 0, 0, $m, 0, $y));//前月末日取得
+                        $wd2 = -$wd1;
                     ?>
+                    
+                    <!--その数だけ空白を表示-->
                     @for ($i = 2; $i <= $wd1; $i++) 
-                        <td></td>
+                        <td class='calendar' style="color: grey">{{ $last_day+2 + $wd2 }}</td>
+                        <?php $wd2 ++; ?>
                     @endfor
                     
                     <!--カレンダー作成-->
@@ -59,11 +65,12 @@
                             //$calendar_expenses = \App\Expense::where('user_id', \Auth::id())->where('day', $day_for_expression)->get()->toArray();
                             //$calendar_expense = array_column( $calendar_expenses, 'money' );
                             $this_day_sum = \App\Expense::where('user_id', \Auth::id())->where('day', $day_for_expression)->sum('money');
+                            $week_number = date('w', mktime(0, 0, 0, $m, $d, $y));
                         ?>   
                         @if($this_day_sum != null)
                             @if($today == $date)
                                 <td class='calendar'>
-                                    <p class='today calendar'>{{ $d }}</p>
+                                    <p class='today calendar youbi_{{ $week_number }}'>{{ $d }}</p>
                                     <?php   
                                         //$spending = number_format($calendar_expense[0]);
                                         $thisDaySum = number_format($this_day_sum);
@@ -72,7 +79,7 @@
                                 </td>
                             @else
                                 <td class='calendar'>
-                                    <p class='calendar'>{{ $d }}</p>
+                                    <p class='calendar youbi_{{ $week_number }}'>{{ $d }}</p>
                                     <?php
                                         $thisDaySum = number_format($this_day_sum);
                                     ?>
@@ -82,19 +89,18 @@
                         @else 
                             @if($today == $date)
                                 <td class='calendar'>
-                                    <p class='today calendar'>{{ $d }}</p>
+                                    <p class='today calendar youbi_{{ $week_number }}'>{{ $d }}</p>
                                     -
                                 </td>
                             @else 
                                 <td class='calendar'>
-                                    <p class='calendar'>{{ $d }}</p>
+                                    <p class='calendar youbi_{{ $week_number }}'>{{ $d }}</p>
                                     -
                                 </td>
                             @endif
                         @endif
                         @if (date("w", mktime(0, 0, 0, $m, $d, $y)) == 0)  <!--0は日曜。W=日曜なら週を終了-->
                             </tr><!--列を閉じる-->
-                            
                             <!-- 次の週がある場合は新たな行を準備-->
                             @if (checkdate($m, $d + 1, $y)) <!--週終了日に一日プラスした日から新しい列をスタート→whileへ-->
                                 <tr class='calendar'>
@@ -105,6 +111,16 @@
                     @endwhile
                     <?php break; ?>
                 @endforeach<!--($expenses as $expense)-->
+                
+                <?php
+                    $wdx = date("w", mktime(0, 0, 0, $m + 1, 0, $y));
+                ?>
+                <!--当月最終日が日曜でなければ、翌月の日にちを表示-->
+                @if(date('w', mktime(0, 0, 0, date($m, $timestamp), date('t', $timestamp), date($y, $timestamp)))!=0)
+                    @for ($i = 1; $i <= 7 - $wdx; $i++)
+                        <td class='calendar' style="color: grey">{{ $i }}</td>
+                    @endfor
+                @endif
             </tr>
         </table><!--出費カレンダーここまで-->
         
@@ -115,8 +131,6 @@
             <tr class='calendar'>
                 @foreach ($expenses as $expense)
                     <?php
-                        $y = substr($ym, 0, 4);//date('Y');
-                        $m = substr($ym, 5, 2);//date('m');
                         $d = 1;
                         $today = date('Y-m-j');
                         // 1日の曜日の前に空白を表示
